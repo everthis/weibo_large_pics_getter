65de86b8b7453ac9c60dfe89787d3c6b364ec2a7
everthis <wwwaap@gmail.com>
2015-04-19 20:58:51 +0800

    files updated.

--- broken_fix.js
+++ broken_fix.js
@@ -6,6 +6,8 @@ var read_data_array_len;
 
 var timer;
 
+var fix_array = [];
+
 
 
 var download = function(url, dest, cb) {
@@ -24,19 +26,22 @@ var ccb = function(str) {
 
 var count = 0;
 function downTimer() {
-	if (count < read_data_array_len) {
-		download(read_data_array[count], count + '.jpg', ccb(count));
-		count += 1;
-		timer = setTimeout(downTimer, 50);
-	};
+  for (var i = 0; i <= fix_array.length - 1; i++) {
+    count = fix_array[i]
+    // if (count < read_data_array_len) {
+      download(read_data_array[count], count + '.jpg', ccb(count));
+      // timer = setTimeout(downTimer, 50);
+    // };
+  };
 }
 
 
-fs.readFile("zhangyuxi.txt", 'utf8', function(err, data) {
+fs.readFile("suo.txt", 'utf8', function(err, data) {
   if (err) throw err;
   read_data_array = JSON.parse(data);
   read_data_array_len = read_data_array.length;
   console.log(read_data_array_len);
+  fix_array = [367,399,407,544,550,654,943];
   downTimer();
 });
 
