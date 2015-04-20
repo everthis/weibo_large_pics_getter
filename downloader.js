9acfd0021776ec8fadf708024bef530c4022cacb
everthis <wwwaap@gmail.com>
2015-04-20 17:21:03 +0800

    files updated.

--- downloader.js
+++ downloader.js
@@ -12,6 +12,7 @@ var DOWNLOAD_DIR = 'hanyujia';
 var read_data_array;
 var read_data_array_len;
 var count = 0;
+var timer;
 
 // Function to download file using HTTP.get
 var download_file_httpget = function(file_url) {
@@ -71,7 +72,7 @@ var download_file_wget = function(file_url) {
     // extract the file name
     var file_name = url.parse(file_url).pathname.split('/').pop();
     // compose the wget command
-    var wget = 'wget -v --show-progress -P ' + DOWNLOAD_DIR + ' ' + file_url;
+    var wget = 'wget -P ' + DOWNLOAD_DIR + ' ' + file_url;
     // excute wget using child_process' exec function
 
     var child = exec(wget, function(err, stdout, stderr) {
@@ -79,7 +80,7 @@ var download_file_wget = function(file_url) {
             throw err;
         } else {
             console.log(file_name + ' downloaded to ' + DOWNLOAD_DIR);
-            startDownload();
+            // startDownload();
         }
     });
 };
@@ -113,6 +114,9 @@ function startDownload() {
         if (count < read_data_array_len) {
             download_file_wget(read_data_array[count]);
             count += 1;
+            timer = setTimeout(startDownload, 300);
+        } else {
+            console.log(count + "loop over!");
         };
     }
     // function startDownload() {
